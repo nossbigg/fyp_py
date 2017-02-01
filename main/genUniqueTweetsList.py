834e82ca344cf7e697b9f8a4bd362ac0d442c9c6
Cheng Gibson <chenggibson@gmail.com>
2017-02-01 23:58:40 +0800

    Add Unique Tweets List generator helper class

---
+++ main/genUniqueTweetsList.py
@@ -0,0 +1,73 @@
+# Generates list of unique tweets and stores it into database
+# TODO modularize this (so that can use with other collections)
+
+import controller.DataMunger as DM
+import model.TweetType as TT
+import pymongo
+
+# retrieve tweets from mongo
+client = pymongo.MongoClient()
+db = client.tweetDb
+collection = db.tweets
+query = collection.find({}, {"id": 1, "tweet_type": 1, "retweeted_status": 1})
+
+# create tweet dictionary
+dataMunger = DM.DataMunger()
+tweetList = list(query)
+tweetDict = dataMunger.buildTweetDictFromList(tweetList)
+
+# generate csv for labelling
+# stores tweets that have already been represented (prevents duplicates of retweets with same parent)
+# tweetsRepresented = {}
+# tweetListExport = []
+# for tweet in tweetDict.values():
+#   tweetId = tweet['id']
+#   # for retweets
+#   if tweet['tweet_type'] == TT.TweetType.RETWEET:
+#     parentTweetId = tweet['retweeted_status']['id']
+#     if parentTweetId not in tweetsRepresented:
+#       tweetsRepresented[parentTweetId] = 0
+#     else:
+#       continue
+#   # for all other types
+#   else:
+#     tweetsRepresented[tweetId] = 0
+#   tweetListExport.append({"id": tweetId})
+
+# generate csv for labelling
+# stores tweets that have already been represented (prevents duplicates of retweets with same parent)
+tweets = {}
+tweetParentToIdLookup = {}
+
+for tweet in tweetDict.values():
+  tweetId = tweet['id']
+  # for retweets
+  if tweet['tweet_type'] == TT.TweetType.RETWEET:
+    parentTweetId = tweet['retweeted_status']['id']
+    if parentTweetId not in tweetParentToIdLookup:
+      tweetParentToIdLookup[parentTweetId] = tweetId
+      tweets[tweetId] = []
+    else:
+      tweets[tweetParentToIdLookup[parentTweetId]].append(tweetId)
+      continue
+  # for all other types
+  else:
+    if tweetId not in tweets:
+      tweets[tweetId] = []
+    else:
+      #FIXME algo when represented tweet meets parent
+      pass
+
+tweetListExport = []
+for key, value in tweets.iteritems():
+  obj = {
+    "id": key,
+    "related_tweets": value
+  }
+  tweetListExport.append(obj)
+
+# write to db (also wipes db)
+collection = db.sickhillaryDbUnique
+collection.remove({})
+collection.insert(tweetListExport)
+client.close()
