c8a34e818e57c2c15804043a84ea14f8d4c5bd5c
Cheng Gibson <chenggibson@gmail.com>
2017-02-02 00:01:12 +0800

    Add tweet type and empty sentiment label to tweet

--- main/importToMongo.py
+++ main/importToMongo.py
@@ -1,18 +1,25 @@
 # Imports unique tweets from source files into mongodb
 
-import os, pymongo
+import pymongo
 from controller import DataMunger as DM
 
 # get unique tweets from source
 dirToSearch = "C:\\Users\\Gibson\\adbpull\\data\\gib-tweet-fyp\\#sickhillary"
-DataMunger = DM.DataMunger(os.getcwd())
-tweetsJsonList = DataMunger.getTweetsFromSource(dirToSearch)
+dataMunger = DM.DataMunger()
+tweetsDict = dataMunger.getTweetsFromSource(dirToSearch)
 
-print(len(tweetsJsonList))
+# label tweets by type
+tweetsDict = dataMunger.labelTweetTypeByList(tweetsDict)
+# add sentiment label field
+tweetsDict = dataMunger.addTweetSentimentLabelToDict(tweetsDict)
+# convert time fields into timestamp
+# TODO convert time fields to mongodb timestamp
+
+# print(len(tweetsDict))
 
 # save to mongo
 client = pymongo.MongoClient()
 db = client.tweetDb
 collection = db.tweets
-result = collection.insert_many(list(tweetsJsonList.values()))
+result = collection.insert(list(tweetsDict.values()))
 client.close()
