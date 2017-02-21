3fb761ac40fb580da9ac551560aa0f336ea090f3
Cheng Gibson <chenggibson@gmail.com>
2017-02-22 03:11:03 +0800

    Add label all tweets back-end executable

---
+++ main/set_tweet_sentiments.py
@@ -0,0 +1,9 @@
+from controller.config import Config
+from controller.data_label_service import DataLabelService
+from controller.database_service import DatabaseService
+
+config = Config()
+dbs = DatabaseService(config)
+dls = DataLabelService(dbs, config)
+
+dls.set_all_sentiments()
