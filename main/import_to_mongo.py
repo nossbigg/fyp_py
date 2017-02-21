24d7feecac30fd3709a2e6a71c780bb25ad155ad
Cheng Gibson <chenggibson@gmail.com>
2017-02-22 03:10:50 +0800

    Add import tweets to mongo back-end executable

---
+++ main/import_to_mongo.py
@@ -0,0 +1,11 @@
+from controller.config import Config
+from controller.data_import_service import DataImportService
+from controller.database_service import DatabaseService
+
+config = Config()
+dbs = DatabaseService(config)
+dis = DataImportService(dbs, config)
+
+# add archives into db (overwrite setting)
+dis.import_from_all_dir(True)
+# dis.import_from_all_dir()
