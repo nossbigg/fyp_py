5ed14ea38cffaf367010fd337275876763f5fd9f
Cheng Gibson <chenggibson@gmail.com>
2017-02-21 15:31:40 +0800

    Add repeated model evaluation + export to csv

--- main/trySvm1.py
+++ main/trySvm1.py
@@ -1,12 +1,14 @@
+import csv
+import itertools
+
 import pandas as pd
 import pymongo
 from controller import PreprocessingUtils as PPU
 from controller.MLUtils import MLUtils
 from controller.NLTKManager import NLTKManager as NLTKMgr
-from sklearn import svm, tree, ensemble, naive_bayes, linear_model, neural_network
+from sklearn import preprocessing
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.model_selection import train_test_split
 
 nltkMgr = NLTKMgr()
 
@@ -21,7 +23,9 @@ collection = db.tweets
 query = collection.find(
   {"id": {"$in": uniqueTweetList},
    "tweet_sentiment_label": {"$ne": ""}},
-  {"id": 1, "text": 1, "tweet_sentiment_label": 1})
+  {"id": 1, "text": 1, "tweet_sentiment_label": 1,
+   "tweet_afinn_label": 1, "tweet_swn_neg_score": 1,
+   "tweet_swn_obj_score": 1, "tweet_swn_pos_score": 1})
 
 # create dataframe
 pdAll = pd.DataFrame(list(query))
@@ -41,10 +45,42 @@ text_tokens = [PPU.PreprocessingUtils.removeTokenByLength(tokens, 3, 999) for to
 # gen global term frequencies
 totalFreq = nltkMgr.genFreqDist([item for sublist in text_tokens for item in sublist])
 termsSelected = PPU.PreprocessingUtils.getTermsWithinStdDev(totalFreq, 3, 3)
-totalFreq.values()
 
 tokens_vec = [nltkMgr.genFreqVector(tokens, termsSelected) for tokens in text_tokens]
 tokens_vec_bin = [nltkMgr.genFreqVector(tokens, termsSelected, True) for tokens in text_tokens]
+
+# normalize tokens_vec
+minNum = 0
+maxNum = -1
+for tokens_vec_sublist in tokens_vec:
+  for count in tokens_vec_sublist:
+    if count > maxNum:
+      maxNum = count
+    elif count < minNum:
+      minNum = count
+min_max_scaler = preprocessing.MinMaxScaler(feature_range=(minNum, maxNum), copy=True)
+tokens_vec = [min_max_scaler.fit_transform(v) for v in tokens_vec]
+
+# append useful features
+min_max_scaler_2 = preprocessing.MinMaxScaler()
+pdAll['tweet_afinn_label'] = min_max_scaler_2.fit_transform(pdAll['tweet_afinn_label'])
+pdAll['tweet_swn_neg_score'] = min_max_scaler_2.fit_transform(pdAll['tweet_swn_neg_score'])
+pdAll['tweet_swn_obj_score'] = min_max_scaler_2.fit_transform(pdAll['tweet_swn_obj_score'])
+pdAll['tweet_swn_pos_score'] = min_max_scaler_2.fit_transform(pdAll['tweet_swn_pos_score'])
+
+tokens_vec = [vec + [a] + [b] + [c] + [d] for vec, a, b, c, d
+              in itertools.izip(tokens_vec,
+                                pdAll['tweet_afinn_label'].tolist(),
+                                pdAll['tweet_swn_neg_score'].tolist(),
+                                pdAll['tweet_swn_obj_score'].tolist(),
+                                pdAll['tweet_swn_pos_score'].tolist())]
+
+tokens_vec_bin = [vec + [a] + [b] + [c] + [d] for vec, a, b, c, d
+                  in itertools.izip(tokens_vec_bin,
+                                    pdAll['tweet_afinn_label'].tolist(),
+                                    pdAll['tweet_swn_neg_score'].tolist(),
+                                    pdAll['tweet_swn_obj_score'].tolist(),
+                                    pdAll['tweet_swn_pos_score'].tolist())]
 
 # store terms
 pdAll['ml_vec'] = tokens_vec
@@ -53,44 +89,29 @@ pdAll['ml_vec_bin'] = tokens_vec_bin
 # prep dataset
 df = pdAll
 df["tweet_sentiment_label2"] = [1 if row['tweet_sentiment_label'] == "s" else 0 for index, row in df.iterrows()]
-train, test = train_test_split(df, test_size=0.2)
-
-train_feature = pd.np.array(train["ml_vec_bin"].tolist())
-train_label = pd.np.array(train["tweet_sentiment_label2"].tolist())
-test_feature = pd.np.array(test["ml_vec_bin"].tolist())
-test_label = pd.np.array(test["tweet_sentiment_label2"].tolist())
 
 # ML
-# Support Vector Machine
-acc = MLUtils.testML(svm.SVC(),
-                     train_feature, train_label, test_feature, test_label)
-print("SVM accuracy: " + acc)
-# Decision Trees
-acc = MLUtils.testML(tree.DecisionTreeClassifier(),
-                     train_feature, train_label, test_feature, test_label)
-print("DT accuracy: " + acc)
-# Logistic Regression
-acc = MLUtils.testML(linear_model.LogisticRegression(),
-                     train_feature, train_label, test_feature, test_label)
-print("LR accuracy: " + acc)
-# Naive Bayes (Bernoulli)
-acc = MLUtils.testML(naive_bayes.BernoulliNB(),
-                     train_feature, train_label, test_feature, test_label)
-print("NB (Bernoulli) accuracy: " + acc)
-# Random Forests
-acc = MLUtils.testML(ensemble.RandomForestClassifier(),
-                     train_feature, train_label, test_feature, test_label)
-print("RFR accuracy: " + acc)
-# Gradient Boosted Trees
-acc = MLUtils.testML(ensemble.GradientBoostingClassifier(),
-                     train_feature, train_label, test_feature, test_label)
-print("GRB accuracy: " + acc)
+ml_list = {
+  "SVM": MLUtils.clfSVC,
+  "DT": MLUtils.clfDT,
+  "LR": MLUtils.clfLR,
+  "NBBernoulli": MLUtils.clfNBBernoulli,
+  "NBMulti": MLUtils.clfNBMultinomial,
+  "RFR": MLUtils.clfRFR,
+  "GRB": MLUtils.clfGRB,
+  "NN": MLUtils.clfNN
+}
 
-# Neural Network
+acc_list = MLUtils.testMultipleMLWithIter(ml_list, 30, df)
 
-acc = MLUtils.testML(neural_network.MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1),
-                     train_feature, train_label, test_feature, test_label)
-print("NN accuracy: " + acc)
+with open('ml_test_keyword.csv', 'wb') as csvfile:
+  writer = csv.writer(csvfile, delimiter=',')
+
+  ml_keys_alphabetical = sorted(ml_list.keys())
+
+  writer.writerow(ml_keys_alphabetical)
+  for acc_sublist in acc_list:
+    writer.writerow(acc_sublist)
 
 # LDA
 # http://scikit-learn.org/stable/auto_examples/applications/topics_extraction_with_nmf_lda.html#sphx-glr-auto-examples-applications-topics-extraction-with-nmf-lda-py
@@ -111,7 +132,6 @@ tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2,
                                 stop_words='english')
 tf = tf_vectorizer.fit_transform(data_samples)
 
-
 tfidf_feature_names = tfidf_vectorizer.get_feature_names()
 
 lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5,
@@ -120,12 +140,14 @@ lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=5,
                                 random_state=0)
 lda.fit(tf)
 
+
 def print_top_words(model, feature_names, n_top_words):
   for topic_idx, topic in enumerate(model.components_):
     print("Topic #%d:" % topic_idx)
     print(" ".join([feature_names[i]
                     for i in topic.argsort()[:-n_top_words - 1:-1]]))
   print()
+
 
 print("\nTopics in LDA model:")
 tf_feature_names = tf_vectorizer.get_feature_names()
@@ -134,5 +156,8 @@ print_top_words(lda, tf_feature_names, n_top_words)
 # TODO use tf-idf to retain valuable but sparse terms
 # TODO use k-fold cv with stratification -> http://scikit-learn.org/stable/modules/cross_validation.html#cross-validation-iterators-for-grouped-data
 # TODO use k-means with elbow method
+# TODO investigate pipelining (see if it helps with anything)
+
+
 
 pass
