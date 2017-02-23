9f40045d4873ef53b1f12a748317415bf0cecfd7
Cheng Gibson <chenggibson@gmail.com>
2017-02-23 17:51:11 +0800

    Add get collection helper function

--- controller/database_service.py
+++ controller/database_service.py
@@ -22,6 +22,12 @@ class DatabaseService:
 
         return self.dbclient[db_name].collection_names()
 
+    def get_collection(self, collection_name, db_name=None):
+        if db_name is None:
+            db_name = self.config.get_db_name()
+
+        return self.dbclient[db_name][collection_name]
+
     def get_unique_tweets_for_collection(self, collection_name, sorted_by_childs_length=False):
         collection_links_name = get_collection_links_name_from_collection_name(collection_name)
 
