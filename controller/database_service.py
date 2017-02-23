78b498882adeea820fb2db48ac71d9600ac83f6e
Cheng Gibson <chenggibson@gmail.com>
2017-02-23 17:44:27 +0800

    Optimize sort algo (retrieve childs only if need to sort)

--- controller/database_service.py
+++ controller/database_service.py
@@ -22,19 +22,25 @@ class DatabaseService:
 
         return self.dbclient[db_name].collection_names()
 
-    def get_unique_tweets_for_collection(self, collection_name):
+    def get_unique_tweets_for_collection(self, collection_name, sorted_by_childs_length=False):
         collection_links_name = get_collection_links_name_from_collection_name(collection_name)
 
+        query_filter = {"_id": 1, "represented_by_id": 1}
+        if sorted_by_childs_length:
+            query_filter["childs"] = 1
+
         cursor = self.get_db()[collection_links_name].find(
-            {},
-            {"_id": 1, "represented_by_id": 1})
+            {}, query_filter)
         tweet_links = list(cursor)
+
+        if sorted_by_childs_length:
+            tweet_links.sort(key=lambda tl: len(tl['childs']), reverse=True)
 
         unique_tweet_list = []
         for doc in tweet_links:
-            if doc['represented_by_id'] is not None:
-                unique_tweet_list.append(doc['represented_by_id'])
-            else:
+            if doc['represented_by_id'] is None:
                 unique_tweet_list.append(doc['_id'])
+            else:
+                unique_tweet_list.append(doc['represented_by_id'])
 
         return unique_tweet_list
