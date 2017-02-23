99f4bdeb84fe3e4101462abd46728d0b9469806b
Cheng Gibson <chenggibson@gmail.com>
2017-02-23 17:01:16 +0800

    Add to_mongo_item helper function Fix static variable bug

--- model/tweet_link.py
+++ model/tweet_link.py
@@ -6,3 +6,11 @@ class TweetLink:
     def __init__(self, id, represented_by_id=None):
         self.id = id
         self.represented_by_id = represented_by_id
+        self.childs = []
+
+    def to_mongo_item(self):
+        return {
+            "_id": self.id,
+            "childs": self.childs,
+            "represented_by_id": self.represented_by_id
+        }
