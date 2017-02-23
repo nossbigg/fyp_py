f1598143bd7e12846148a0e402de14574730db13
Cheng Gibson <chenggibson@gmail.com>
2017-02-23 16:07:46 +0800

    Add get unique tweets helper function

--- controller/database_service.py
+++ controller/database_service.py
@@ -1,4 +1,5 @@
 import pymongo
+from controller.database_utils import get_collection_links_name_from_collection_name
 
 
 class DatabaseService:
@@ -20,3 +21,20 @@ class DatabaseService:
             db_name = self.config.get_db_name()
 
         return self.dbclient[db_name].collection_names()
+
+    def get_unique_tweets_for_collection(self, collection_name):
+        collection_links_name = get_collection_links_name_from_collection_name(collection_name)
+
+        cursor = self.get_db()[collection_links_name].find(
+            {},
+            {"_id": 1, "represented_by_id": 1})
+        tweet_links = list(cursor)
+
+        unique_tweet_list = []
+        for doc in tweet_links:
+            if doc['represented_by_id'] is not None:
+                unique_tweet_list.append(doc['represented_by_id'])
+            else:
+                unique_tweet_list.append(doc['_id'])
+
+        return unique_tweet_list
