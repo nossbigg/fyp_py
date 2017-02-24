56720c9004fe089dd15cb7e5b3550e4f9d3a0926
Cheng Gibson <chenggibson@gmail.com>
2017-02-24 19:42:08 +0800

    Add get top terms of collections (experimental)

---
+++ experimental/get_top_terms_tf.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from controller.config import Config
+from controller.database_service import DatabaseService
+from controller.database_utils import get_collection_links_name_from_collection_name, get_tweet_collections_only
+from controller.nltk_manager import NLTKManager
+from controller.preprocessing_utils import PreprocessingUtils as PPU
+
+config = Config()
+dbs = DatabaseService(config)
+
+db = dbs.get_db()
+collection_names = get_tweet_collections_only(dbs.get_collection_names())
+for collection_name in collection_names:
+    # get tweets
+    unique_tweet_list = dbs.get_unique_tweet_ids_for_collection(collection_name, True)
+
+    tweets = db[collection_name].find(
+        {"_id": {"$in": unique_tweet_list}},
+        {"_id": 1, "text": 1, "tweet_sentiment_label": 1,
+         "tweet_score_afinn": 1, "tweet_score_swn_pos": 1,
+         "tweet_score_swn_neg": 1, "tweet_score_swn_obj": 1})
+    tweets = list(tweets)
+
+    pdAll = pd.DataFrame(tweets)
+
+    # text preprocessing
+    nltk_mgr = NLTKManager()
+    text_tokens = [nltk_mgr.tokenize(text) for text in pdAll['text']]
+    text_tokens = [PPU.removeTokenByLength(tokens, 3, 20) for tokens in text_tokens]
+    text_tokens = [PPU.removeTokenHTTP(tokens) for tokens in text_tokens]
+    text_tokens = [PPU.removeTokenUserReference(tokens) for tokens in text_tokens]
+    text_tokens = [PPU.removeTokenHashtag(tokens) for tokens in text_tokens]
+    text_tokens = [nltk_mgr.remove_stopword_tokens(tokens) for tokens in text_tokens]
+    text_tokens = [nltk_mgr.lemmatize_tokens(tokens) for tokens in text_tokens]
+
+    # gen global term frequencies
+    totalFreq = nltk_mgr.gen_freq_dist([item for sublist in text_tokens for item in sublist])
+    termsSelected = PPU.getTermsWithinStdDev(totalFreq, -1, 3)
+
+    print("Top terms for '" + collection_name + "' : " + " ,".join(termsSelected))
+
+    # TODO look at tfidfvectorizer, preprocessor
+    pass
