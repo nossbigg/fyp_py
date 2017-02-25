6e86967f498d1fca9e0d79a7fcb1934bdd5c2d5b
Cheng Gibson <chenggibson@gmail.com>
2017-02-25 21:55:48 +0800

    Split set sentiments function to tweet_label/tweet_sentiment_scores

--- controller/data_label_service.py
+++ controller/data_label_service.py
@@ -1,5 +1,6 @@
 from controller.database_utils import *
 from controller.sentiment_utils import *
+from controller.tweet_llinks_utils import gen_represented_by_reverse_lookup_dict
 
 
 class DataLabelService:
@@ -9,6 +10,16 @@ class DataLabelService:
     def __init__(self, database_service_class, config_class):
         self.dbclient = database_service_class
         self.config = config_class
+
+    def set_all_tweets_type(self):
+        db = self.dbclient.get_db()
+
+        # get tweet collection names
+        collection_names = get_tweet_collections_only(db.collection_names())
+
+        # iterate through all collections
+        for collection_name in collection_names:
+            self.set_all_tweet_type_collection(collection_name)
 
     def set_all_sentiments(self):
         db = self.dbclient.get_db()
@@ -21,34 +32,65 @@ class DataLabelService:
             self.set_all_sentiments_collection(collection_name)
 
     def set_all_sentiments_collection(self, collection_name):
-        collection = self.dbclient.get_db()[collection_name]
+        """
+        Set text sentiment scores
+        Utilizes tweet links db to speed up process
 
+        :param collection_name:
+        :return:
+        """
+        # get links information
+        collection_link_name = get_collection_links_name_from_collection_name(collection_name)
+        cursor = self.dbclient.get_collection(collection_link_name).find({})
+        tweet_links = {l["_id"]: l for l in list(cursor)}
+        retweet_reverse_lookup_list = gen_represented_by_reverse_lookup_dict(tweet_links.values())
+
+        # get cursor to label tweets
+        unique_tweet_list = gen_unique_tweet_ids_from_links(tweet_links.values())
+        collection = self.dbclient.get_collection(collection_name)
         cursor = collection.find(
-            {},
-            {"_id": 1, "text": 1, 'retweeted_status': 1, 'is_quote_status': 1, 'quoted_status_id': 1}
-        )
+            {"_id": {"$in": unique_tweet_list}},
+            {"_id": 1, "text": 1, 'tweet_type': 1})
 
         for tweet in cursor:
             tweet_id = tweet['_id']
             tweet_text = tweet['text']
 
-            tweet_type = get_tweet_type(tweet)
             score_afinn = get_score_afinn(tweet_text)
             score_pos, score_neg, score_obj = get_scores_swn(tweet_text)
 
-            result = collection.update(
-                {"_id": tweet_id},
+            ids_to_update = [tweet_id]
+            if tweet_id in retweet_reverse_lookup_list:
+                reverse_lookup_id = retweet_reverse_lookup_list[tweet_id]
+                childs = tweet_links[reverse_lookup_id]["childs"]
+                ids_to_update += childs
+
+            collection.update(
+                {"_id": {"$in": ids_to_update}},
                 {'$set': {
-                    "tweet_type": tweet_type,
                     "tweet_score_afinn": score_afinn,
                     "tweet_score_swn_pos": score_pos,
                     "tweet_score_swn_neg": score_neg,
                     "tweet_score_swn_obj": score_obj
                 }},
-                multi=False
-            )
+                multi=True)
 
-            # TODO parallelize afinn labelling process
+    def set_all_tweet_type_collection(self, collection_name):
+        collection = self.dbclient.get_db()[collection_name]
+
+        cursor = collection.find(
+            {},
+            {"_id": 1, 'retweeted_status': 1,
+             'is_quote_status': 1, 'quoted_status_id': 1})
+
+        for tweet in cursor:
+            tweet_id = tweet['_id']
+            tweet_type = get_tweet_type(tweet)
+
+            collection.update(
+                {"_id": tweet_id},
+                {'$set': {"tweet_type": tweet_type}},
+                multi=False)
 
     def remove_all_sentiments(self):
         db = self.dbclient.get_db()
