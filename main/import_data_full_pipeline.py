1ac1a04cbbe5e383b61615c9e928037f117509e5
Cheng Gibson <chenggibson@gmail.com>
2017-02-26 16:23:06 +0800

    Add generation of tweet labeling csvs as part of pipeline

--- main/import_data_full_pipeline.py
+++ main/import_data_full_pipeline.py
@@ -2,6 +2,7 @@ from controller.config import Config
 from controller.data_import_service import DataImportService
 from controller.data_label_service import DataLabelService
 from controller.database_service import DatabaseService
+from controller.tweet_csv_label_service import TweetCSVLabelService
 from controller.tweet_links_service import TweetLinksService
 
 config = Config()
@@ -22,4 +23,6 @@ tls.gen_all_collection_links(True)
 # Label tweet text sentiments
 dls.set_all_sentiments()
 
-
+# Gen csvs for labelling
+tcls = TweetCSVLabelService(dbs, config)
+tcls.gen_csv_label_docs(overwrite=False, exclude_labelled=True)
