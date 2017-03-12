65032defd2dd2c032e93db964b5aa9e1e6b098fe
Cheng Gibson <chenggibson@gmail.com>
2017-03-13 01:41:20 +0800

    Fix absolute/relative path problem for config directories

--- controller/config.py
+++ controller/config.py
@@ -1,3 +1,4 @@
+import os
 from ConfigParser import ConfigParser
 from os.path import dirname, abspath
 
@@ -19,13 +20,26 @@ class Config:
 
     # TODO add logic to handle relative/absolute path
     def get_corpus_dir(self):
-        return self.config_var.get('CORPUS', 'CORPUS_DIR')
+        path = self.config_var.get('CORPUS', 'CORPUS_DIR')
+        return self.__convert_to_abs_path(path)
 
     def get_tweet_csv_label_dir(self):
-        return self.config_var.get('CORPUS', 'TWEET_CSV_LABEL_DIR')
+        path = self.config_var.get('CORPUS', 'TWEET_CSV_LABEL_DIR')
+        return self.__convert_to_abs_path(path)
 
     def get_nps_tag_convert_filepath(self):
-        return self.config_var.get('CORPUS', 'NPS_TAG_CONVERT_PATH')
+        path = self.config_var.get('CORPUS', 'NPS_TAG_CONVERT_PATH')
+        return self.__convert_to_abs_path(path)
+
+    def get_ml_test_results_dir(self):
+        path = self.config_var.get('ML-TEST', 'ML_TEST_RESULTS_DIR')
+        return self.__convert_to_abs_path(path)
+
+    @staticmethod
+    def __convert_to_abs_path(path):
+        if not os.path.isabs(path):
+            path = os.path.join(ROOT_DIR, path)
+        return path
 
     @staticmethod
     def get_root_dir():
