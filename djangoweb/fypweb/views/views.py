9fcd0c5eeaed0f2ff3f466891640f2025e894702
Cheng Gibson <chenggibson@gmail.com>
2017-03-12 14:18:34 +0800

    Remove django web app (decided to create separate project)

--- djangoweb/fypweb/views/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    test_var = "This is a test var"
-
-    context = {"test_var": test_var}
-    return render(request, 'index.html', context)
